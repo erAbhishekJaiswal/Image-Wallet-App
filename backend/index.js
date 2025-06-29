055559452d282b2fd318b85c5ab8656c1e0be0f3
Abhishek Jaiswal <er.abhishek72@gmail.com>
2025-06-29 14:34:46 +0530

    new update

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const studentRoutes = require('./routes/studentRoutes');
+// const studentRoutes = require('./routes/studentRoutes');
+const imageRoutes = require('./routes/imageRoutes');
 
 const app = express();
 connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads')); // Serve uploaded files
-app.use('/api/students', studentRoutes);
+// app.use('/uploads', express.static('uploads')); // Serve uploaded files
+app.use('/api/image', imageRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API running');
+});
 
 const PORT = process.env.PORT || 5000;
 
